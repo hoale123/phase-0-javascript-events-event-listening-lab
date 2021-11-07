ef5b4c428736668a87084e2812563ee485d858ba
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-07 12:55:48 -0500

    commit tree 1b3f9cd20fbe6586e00383dccde78a4fbed90ff5

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('input')
 function addingEventListener() {
+    //make the alertclick function
+   const alertCLick =  () => alert('I was clicked')
+     // add and event to the input, in this case when its click, alert with the log; 'I was clicked'
+     input.addEventListener('click', alertCLick)
 }
+// call the addEventListener()
+addingEventListener()
